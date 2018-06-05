155e1a2db7ed77c34838e11f8501a4615aff7331
Harkline <35963363+Harkline@users.noreply.github.com>
2018-06-05 13:37:21 +0000

    Add files via upload

--- src/vue/PanelAffichageFrise.java
+++ src/vue/PanelAffichageFrise.java
@@ -37,26 +37,34 @@ public class PanelAffichageFrise extends JPanel implements ConstantesTextes {
 		
 		monModele = new ModeleTable(parFrise);
 		
+		//Création d'une JTable et de toute ces caractéristiques
 		tableFrise = new JTable(monModele);
 		tableFrise.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		tableFrise.getTableHeader().setReorderingAllowed(false);
 		tableFrise.setRowHeight(80);
 		tableFrise.setDefaultRenderer(Evenement.class, new CelluleRenderer());
 		
+		//On créer un JScrollPane
 		scrollPane = new JScrollPane(tableFrise,
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
-				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED); 
 		
-		scrollPane.setPreferredSize(new Dimension(360, 360));
+		scrollPane.setPreferredSize(new Dimension(360, 360));//On lui définie une taille par défault
 		
-		add(scrollPane, BorderLayout.CENTER);
+		add(scrollPane, BorderLayout.CENTER);//Puis on l'ajoute au BorderLayout
 		
+		//Création d'un popupMenu
 		JPopupMenu popupMenu = new JPopupMenu("Outils");
 		itemModifier = new JMenuItem(AFFICHAGE_POPUPMENU_MODIFIER);
 		itemSupprimer = new JMenuItem(AFFICHAGE_POPUPMENU_SUPPRIMER);
 		
+		//Ajout des items 
 		popupMenu.add(itemModifier);
 		popupMenu.add(itemSupprimer);
+		
+		/**
+		*Méthode qui permet d'utiliser MouseListener sur la tableFrise 
+		*/
 		
 		tableFrise.addMouseListener(new MouseAdapter() {
 			
@@ -73,6 +81,10 @@ public class PanelAffichageFrise extends JPanel implements ConstantesTextes {
 
 			}
 			
+			/**
+			* Méthode qui permet de savoir s'il y a un eu un clic sur la JTable 
+			*/
+			
 			public void mousePressed(MouseEvent evt) {
 				
 				JTable table = (JTable) evt.getSource();
@@ -85,6 +97,10 @@ public class PanelAffichageFrise extends JPanel implements ConstantesTextes {
 		        	popupMenu.show(evt.getComponent(), evt.getX(), evt.getY());
 		        }
 		      }
+			
+			/**
+			* Méthode qui permet de savoir si le clic de la souris est fini 
+			*/
 			
 			public void mouseReleased(MouseEvent evt) {
 				
@@ -103,50 +119,103 @@ public class PanelAffichageFrise extends JPanel implements ConstantesTextes {
 		
 	}
 	
+	/**
+	* Accesseur de la ModeleTable
+	* @return une ModeleTable 
+	*/
+	
 	public ModeleTable getMonModele() {
 		return monModele;
 	}
+	
+	/**
+	* Modifieur de la ModeleTable 
+	*/
 
 	public void setMonModele(ModeleTable monModele) {
 		this.monModele = monModele;
 	}
+	
+	/**
+	* Accesseur du JScrollPane
+	* @return JScrollPane
+	*/
 
 	public JScrollPane getScrollPane() {
 		return scrollPane;
 	}
+	
+	/**
+	* Modifieur du JScrollPane
+	*/
 
 	public void setScrollPane(JScrollPane scrollPane) {
 		this.scrollPane = scrollPane;
 	}
 
+	/**
+	* Accesseur pour obtenir une ligne de la JTable
+	* @return rowIndex qui est l'indice d'une ligne
+	*/
+	
 	public int getRowIndex() {
 		return rowIndex;
 	}
+	
+	/**
+	* Modifieur qui permet de changer l'indice d'une ligne de la JTable 
+	*/
 
 	public void setRowIndex(int rowIndex) {
 		this.rowIndex = rowIndex;
 	}
+	
+	/**
+	* Accesseur pour obtenir une colonne de la JTable
+	* @return colIndex qui est l'indice de la colonne
+	*/
 
 	public int getColIndex() {
 		return colIndex;
 	}
+	
+	/**
+	* Modifieur qui permet de changer l'indice d'une colonne de la JTable 
+	*/
 
 	public void setColIndex(int colIndex) {
 		this.colIndex = colIndex;
 	}
+	
+	/**
+	* Méthode qui permet de synchroniser l'écoute sur le contrôleur des items du popmenu
+	*/
 
 	public void enrengistreEcouteur(Controleur parC) {
 		itemModifier.addActionListener(parC);
 		itemSupprimer.addActionListener(parC);
 	}
+	
+	/**
+	* Accesseur qui permet d'obtenir la tableFrise 
+	* @return tableFrise
+	*/
 
 	public JTable getTableFrise() {
 		return tableFrise;
 	}
+	
+	/**
+	* Modifieur qui permet de changer la tableFrise
+	*/
 
 	public void setTableFrise(JTable tableFrise) {
 		this.tableFrise = tableFrise;
 	}
+	
+	/**
+	* Méthode qui permet de mettre a jour la JTable 
+	*/
 	
 	public void updateTable(FriseChronologique parFrise) {
 		monModele = new ModeleTable(parFrise);
