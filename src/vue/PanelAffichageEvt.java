f8aa420af10275d5965eeba7460e3a9c9c610341
Harkline <35963363+Harkline@users.noreply.github.com>
2018-06-05 12:38:49 +0000

    Update PanelAffichageEvt.java

--- src/vue/PanelAffichageEvt.java
+++ src/vue/PanelAffichageEvt.java
@@ -10,20 +10,23 @@ public class PanelAffichageEvt extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	
-	JLabel labels[] = new JLabel[4];
+	JLabel labels[] = new JLabel[4]; //Création d'un tableau de JLabel, qui contiendra la date, le titre, la description et l'image associé à l'évènement
 	
 	public PanelAffichageEvt(Evenement parEvt) {
-				
+		
+		//Permet d'obtenir l'affichage au niveau texte des évènements dans le cardLayout avec la date, le titre et la description de ceux-ci 		
 		labels[0] = new JLabel(parEvt.getDate().toString());
 		labels[1] = new JLabel("<html><h1>" + parEvt.getTitre() + "</h1></html>");
 		labels[2] = new JLabel(parEvt.getChDescription());
 		
+		//Si l'évènement a une image qui lui est associée, on la lui attribue
 		if (!parEvt.getChPhoto().isEmpty()) {
 			labels[3] = new JLabel("");
 			labels[3].setIcon(new ImageIcon(parEvt.getChPhoto()));
 			//add(labels[3]);
 		}
-		
+	
+		//on ajoute tout ça pour pouvoir l'afficher réellement !
 		for (int i=0;i!=3;i++) {
 			add(labels[i]);
 		}
